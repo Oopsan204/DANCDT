fbedfe01c565763922f82d8c2250655933caea6b
Oopsan204 <22010740@st.phenikaa-uni.edu.vn>
2026-04-29 09:29:12 +0700

    fix buffer 3

--- GantrySCADA/obj/Debug/net8.0-windows/GantrySCADA.AssemblyInfo.cs
+++ GantrySCADA/obj/Debug/net8.0-windows/GantrySCADA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GantrySCADA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53087314587e7ec741d6d6c13153fc1388c5b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fb4e5f0e1f5992100eda0215ef962033710a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GantrySCADA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GantrySCADA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
